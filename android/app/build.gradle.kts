d8665d15d4e074c35a147fbd42bce95102a9bfd7
Diego Torres <datorres@ucompensar.edu.co>
2025-06-09 17:39:08 -0500

    Firebase projec was added, app was renaned, db/auth was migrated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.attendify"
+    namespace = "com.edupilot360.attendify"
     // compileSdk = flutter.compileSdkVersion
     compileSdk = 35
     // ndkVersion = flutter.ndkVersion
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.attendify"
+        applicationId = "com.edupilot360.attendify"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // minSdk = flutter.minSdkVersion
